3c2404fa14b431268646f3d16ee9584b6db99bfa
Laurenfitzz <113629592+laurenfitzz@users.noreply.github.com>
2023-04-07 16:26:45 -0400

    Add files via upload

--- better_ebirdAPI.py
+++ better_ebirdAPI.py
@@ -1,13 +1,27 @@
-better_ebirdAPI
+name = "better_ebirdAPI"
+author='Lauren Fitzgerald'
+descrpition = "This module was created for bird watchers! To use better_ebirrdAPI the user requires an ebirdAPI 2.0 key"
+install_requires=[
+        "pandas",
+    "IPython", 
+    "geopandas",
+    "requests",
+    "bs4",
+    "json",
+    "folium",
+    "requests",
+    "google_images_search"]
+
 
 #1 common to scientific 
 # this function can be used to retreive the scientific name of a bird using the common name as input
 # for this function to work the user must download the "PFW-Species-translation-table.csv" and read it as species_data 
 #use the following:
-#species_data = pd.read_csv("PFW-species-translation-table.csv")
 def com_to_sci(common_name):
     # make it not case sensitve 
     common_name = common_name.lower()
+    import pandas as pd
+    species_data = pd.read_csv("PFW-species-translation-table.csv")
     scientific_name = species_data.loc[species_data['american_english_name'].str.lower() == common_name, 'scientific_name'].values[0]
     return scientific_name
 
@@ -16,6 +30,8 @@ def com_to_sci(common_name):
 #Like com_to sci the user must download the "PFW-Species-translation-table.csv" and read it as species_data 
 def sci_to_com(scientific_name):
     scientific_name = scientific_name.lower()
+    import pandas as pd
+    species_data = pd.read_csv("PFW-species-translation-table.csv")
     common_name = species_data.loc[species_data['scientific_name'].str.lower() == scientific_name, 'american_english_name'].values[0]
     return(common_name)
 
@@ -23,6 +39,8 @@ def sci_to_com(scientific_name):
 #this function works to match names and get a list of possible sub species such as a list of all names containing duck or wren, 
 #this function isnt perfect as it can miss related speices that do not contain matching names, however its a good indicator
 def list_types(sub_species):
+    import pandas as pd
+    species_data = pd.read_csv("PFW-species-translation-table.csv")
     matching_names = species_data[species_data['american_english_name'].str.contains(sub_species, case=False)]['american_english_name'].tolist()
     return matching_names
 
@@ -33,7 +51,10 @@ def list_types(sub_species):
 def com_des(common_name):
     common_name = common_name.replace(" ", "-")
     url = f"https://www.audubon.org/field-guide/bird/{common_name}"
+    import requests
     response = requests.get(url)
+    import bs4
+    from bs4 import BeautifulSoup
     soup = BeautifulSoup(response.text, "html.parser")
     bird_div = soup.find("div", class_="bird_discussion")
     if bird_div is not None:
@@ -47,7 +68,10 @@ def com_des(common_name):
 def sci_des(scientific_name):
     scientific_name = scientific_name.replace(" ", "_")
     url = f"https://picturebirdai.com/wiki/{scientific_name}.html"
+    import requests
     response = requests.get(url)
+    import bs4
+    from bs4 import BeautifulSoup
     soup = BeautifulSoup(response.text, "html.parser")
     bird_div = soup.find("div", {"id": "description-content"})
     if bird_div is not None:
@@ -63,7 +87,10 @@ def sci_des(scientific_name):
 def bird_description(common_name):
     common_name1 = common_name.replace(" ", "-")
     url = f"https://www.audubon.org/field-guide/bird/{common_name1}"
+    import requests
     response = requests.get(url)
+    import bs4
+    from bs4 import BeautifulSoup
     soup = BeautifulSoup(response.text, "html.parser")
     bird_div = soup.find("div", class_="bird_discussion")
     if bird_div is not None:
@@ -89,7 +116,10 @@ def bird_description(common_name):
 #7 Google API PIC 
 #This function retreives pictures using Google Image Search. It works great for those who have access to a premium google api key
 #for those who do not have access to a google api key there is another function, "wiki_pic" which scrapes wikipedia for bird images. 
-def google_API_pic(common_name):
+def google_API_pic(common_name, google_apikey, google_CX):
+    import requests
+    import google_images_search
+    from google_images_search import GoogleImagesSearch
     # Initialize GoogleImagesSearch object
     gis = GoogleImagesSearch(google_apikey, google_CX)
     # Set search parameters
@@ -104,13 +134,18 @@ def google_API_pic(common_name):
     image_url = gis.results()[0].url
     pic = requests.get(image_url)
     # Display image in notebook
+    import IPython 
+    from IPython.display import Image
     return Image(pic.content)
 
 #8
 #This function retreives pictures using Google Image Search and uses base64 to encode the image for the later mapping functions
 #It works great for those who have access to a premium google api key, however wiki_pic_4map works interchangebly for those who do not have an google API key
 #this function is more reliable at loading the images, and it is reccomended to use this 
-def google_API_pic4map(common_name):
+def google_API_pic4map(common_name, google_apikey, google_CX):
+    import requests
+    import google_images_search
+    from google_images_search import GoogleImagesSearch
     # Initialize GoogleImagesSearch object
     gis = GoogleImagesSearch(google_apikey, google_CX)
 
@@ -125,6 +160,7 @@ def google_API_pic4map(common_name):
     gis.search(search_params=_search_params)
     image_data = requests.get(gis.results()[0].url).content
     # Encode image data as base64
+    import base64
     image_base64 = base64.b64encode(image_data).decode()
     
     return image_base64
@@ -132,11 +168,15 @@ def google_API_pic4map(common_name):
 #9 Wiki picture
 #this is used to retieve a bird image for those who do not have access to a google API key and therefore cannot use the function "google_API_pic"
 def wiki_pic(common_name):
+    import requests
     # Make a GET request to the Wikipedia page
-    url = f"https://en.wikipedia.org/wiki/{common_name}"
+    sci_name = com_to_sci(common_name)
+    url = f"https://en.wikipedia.org/wiki/{sci_name}"
     common_name = common_name.replace(" ", "_")
     response = requests.get(url)
     # Parse the HTML content using BeautifulSoup
+    import bs4
+    from bs4 import BeautifulSoup
     soup = BeautifulSoup(response.text, "html.parser")
     # Find the infobox table with either class name
     infobox = soup.find("table", class_=["infobox biota", "taxobox"])
@@ -163,15 +203,21 @@ def wiki_pic(common_name):
         else:
             return "image not found"  
     #return(picture_url)
+    import IPython 
+    from IPython.display import Image
     display(Image(url=picture_url))
 
 #10 
 #this is used to retieve a bird image for those who do not have access to a google API key and therefore cannot use the function "google_API_pic4map"
 def wiki_pic_map(common_name):
+    import requests
+    import base64
     # Make a GET request to the Wikipedia page
     url = f"https://en.wikipedia.org/wiki/{common_name}"
     common_name = common_name.replace(" ", "_")
     response = requests.get(url)
+    import bs4
+    from bs4 import BeautifulSoup
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.text, "html.parser")
     # Find the infobox table with either class name
@@ -204,9 +250,12 @@ def wiki_pic_map(common_name):
         else:
             return None 
 
+        
 #11 retieves species data 
 #this uses the ebirdAPI 2.0 to summarize and retieve the information about the bird using the scientfic name 
-def retrieve_species_data(scientific_name):
+def retrieve_species_data(scientific_name, ebird_api_key):
+    import requests
+    import json
     url = "https://api.ebird.org/v2/ref/taxonomy/ebird"
     params = {
         "fmt": "json",
@@ -225,9 +274,11 @@ def retrieve_species_data(scientific_name):
 
 
 #12 this maps nearby observations and uses the functions bird_description, and 
-def nearby_observations(lat, lng, distance_km):
+def nearby_observations(lat, lng, distance_km, ebird_api_key):
+    import requests
+    import folium
     # Define the API endpoint URL
-    #dis = 15km 
+    import json
     url = f'https://api.ebird.org/v2/data/obs/geo/recent?lat={lat}&lng={lng}&dist={distance_km}&cat=species&fmt=json'
 
     #API inp
@@ -248,6 +299,7 @@ def nearby_observations(lat, lng, distance_km):
             
  # Get bird description
         bird_description_text = bird_description(bird_name)
+        import base64
         bird_image_data = wiki_pic_map(bird_name)
         
         #HTML popup
@@ -265,17 +317,21 @@ def nearby_observations(lat, lng, distance_km):
     return bird_map
 
 #13 Same this as nearby_observations, but uses google images API
-def nearby_observations_GAPI(lat, lng, distance_km):
+def nearby_observations_GAPI(lat, lng, distance_km, ebird_api_key):
+    import json
+    
     # Define the API endpoint URL
     #dis = 15km 
     url = f'https://api.ebird.org/v2/data/obs/geo/recent?lat={lat}&lng={lng}&dist={distance_km}&cat=species&fmt=json'
 
     #API inp
     headers = {'X-eBirdApiToken': ebird_api_key}
+    import requests
     response = requests.get(url, headers=headers)
 
     data = response.json()
     
+    import folium
     bird_map = folium.Map(location=[lat, lng], zoom_start=10)
 
     # Loop through the data and add markers for each bird sighting
@@ -298,7 +354,6 @@ def nearby_observations_GAPI(lat, lng, distance_km):
         <p><b>Date:</b> {obs_date}</p>
         <p><b>Description:</b> {bird_description_text}</p>
         """
-        
         marker = folium.Marker(location=location, popup=folium.Popup(popup_html, max_width=400))
         marker.add_to(bird_map)
 
@@ -306,10 +361,13 @@ def nearby_observations_GAPI(lat, lng, distance_km):
 
 #14
 #this searches for a specific bird using its common name in a specified radius 
-def nearby_bird(lat, lng, distance_km, bird_name):
+def nearby_bird(lat, lng, distance_km, scientific_name, ebird_api_key):
+    import requests
+    import json
+    import folium
     # Define the API endpoint URL
     #dis = 15km
-    url = f'https://api.ebird.org/v2/data/obs/geo/recent?lat={lat}&lng={lng}&dist={distance_km}&sci={bird_name}&fmt=json'
+    url = f'https://api.ebird.org/v2/data/obs/geo/recent?lat={lat}&lng={lng}&dist={distance_km}&sci={scientific_name}&fmt=json'
 
     headers = {'X-eBirdApiToken': ebird_api_key}
     response = requests.get(url, headers=headers)
@@ -344,10 +402,13 @@ def nearby_bird(lat, lng, distance_km, bird_name):
 
 #15 nearby_bird_GAPI
 # does the same thing as nearby_bird but with the google api key 
-def nearby_bird_GAPI(lat, lng, distance_km, bird_name):
+def nearby_bird_GAPI(lat, lng, distance_km, scientific_name, ebird_api_key):
+    import requests
+    import json
+    import folium
     # Define the API endpoint URL
     #dis = 15km
-    url = f'https://api.ebird.org/v2/data/obs/geo/recent?lat={lat}&lng={lng}&dist={distance_km}&sci={bird_name}&fmt=json'
+    url = f'https://api.ebird.org/v2/data/obs/geo/recent?lat={lat}&lng={lng}&dist={distance_km}&sci={scientific_name}&fmt=json'
 
     headers = {'X-eBirdApiToken': ebird_api_key}
     response = requests.get(url, headers=headers)
@@ -382,7 +443,10 @@ def nearby_bird_GAPI(lat, lng, distance_km, bird_name):
 
 #16 notable bird 
 # this finds the notable observation of rare or unique birds within a specified radius of inputed coordinates 
-def notable_bird(lat, lng, distance_km):
+def notable_bird(lat, lng, distance_km, ebird_api_key):
+    import json
+    import requests
+    import folium
     # Define the API endpoint URL
     #dis = 25km
     url =f'https://api.ebird.org/v2/data/obs/geo/recent/notable?lat={lat}&lng={lng}&dist={distance_km}&fmt=json'
@@ -426,7 +490,10 @@ def notable_bird(lat, lng, distance_km):
 
 #17 notable_bird_GAPI
 # does the same thing as notable_bird but uses the google api 
-def notable_bird_GAPI(lat, lng, distance_km):
+def notable_bird_GAPI(lat, lng, distance_km, ebird_api_key):
+    import json
+    import requests
+    import folium
     # Define the API endpoint URL
     #dis = 25km
     url =f'https://api.ebird.org/v2/data/obs/geo/recent/notable?lat={lat}&lng={lng}&dist={distance_km}&fmt=json'
@@ -470,7 +537,10 @@ def notable_bird_GAPI(lat, lng, distance_km):
 
 # "Hotspots represent a set of public locations that people regularly visit for birding, 
 # regardless of how amazing they are for birds. The primary requirement of a Hotspot is that it is publicly accessible
-def gethotspot(lat, lng):
+def gethotspot(lat, lng, ebird_api_key):
+    import requests
+    import json
+    import folium
     url = f'https://api.ebird.org/v2/ref/hotspot/geo?lat={lat}&lng={lng}&dist=5&fmt=json'
 
     payload={}
